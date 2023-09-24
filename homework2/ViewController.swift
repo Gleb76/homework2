3815849c1c6d965d9b50231df7c68f5e3e4bfcf7
Gleb Klyga <klyga90@bk.ru>
2023-09-24 20:00:00 +0300

    Initial Commit

---
+++ homework2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  homework2
+//
+//  Created by Глеб Клыга on 24.09.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
